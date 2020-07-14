7c0888f766dbdf9f6d95dfa0dbfb742b58208e4e
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-14 17:57:02 +0530

    remove import from utils

--- felicette/landsat_handler.py
+++ felicette/landsat_handler.py
@@ -1,7 +1,5 @@
 from satsearch import Search
 from felicette.utils.get_wrs import get_tiny_bbox
-from felicette.utils.landsat_util import find_first_landsat
-
 
 search = Search(bbox=get_tiny_bbox(85.8245, 20.2961), query = {
   "eo:cloud_cover": {
