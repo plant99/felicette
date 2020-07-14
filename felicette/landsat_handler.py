c7ef89823d98403f3253099d32a665a5cae7db16
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-14 19:31:03 +0530

    Initialize file manager

--- felicette/landsat_handler.py
+++ felicette/landsat_handler.py
@@ -1,5 +1,8 @@
+import requests
+
 from satsearch import Search
 from felicette.utils.get_wrs import get_tiny_bbox
+from felicette.utils.file_manager import save_to_file, data_file_exists
 
 search = Search(bbox=get_tiny_bbox(85.8245, 20.2961), query = {
   "eo:cloud_cover": {
@@ -10,4 +13,13 @@ search = Search(bbox=get_tiny_bbox(85.8245, 20.2961), query = {
   }
 })
 landsat_item = search.items()[0]
-print(landsat_item.assets)
+
+
+
+# save bands 2,3,4
+band2_filename = landsat_item._data["id"] + "-b2"
+if not data_file_exists(band2_filename):
+	r = requests.get(landsat_item.assets["B2"]["href"])
+	save_to_file(r.content, band2_filename)
+else:
+	print("required data exists")
