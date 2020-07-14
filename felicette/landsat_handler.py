d543380cf755506ae0b12b0c8dddec3ea9ef6b23
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-14 21:35:25 +0530

    Download 2,3,4 bands

--- felicette/landsat_handler.py
+++ felicette/landsat_handler.py
@@ -1,5 +1,3 @@
-import requests
-
 from satsearch import Search
 from felicette.utils.get_wrs import get_tiny_bbox
 from felicette.utils.file_manager import save_to_file, data_file_exists
@@ -12,9 +10,21 @@ landsat_item = search.items()[0]
 
 
 # save bands 2,3,4
-band2_filename = landsat_item._data["id"] + "-b2"
+band2_filename = landsat_item._data["id"] + "-b2.tiff"
 if not data_file_exists(band2_filename):
-    r = requests.get(landsat_item.assets["B2"]["href"])
-    save_to_file(r.content, band2_filename)
+    save_to_file(landsat_item.assets["B2"]["href"], band2_filename)
 else:
-    print("required data exists")
+    print("required data exists for {}".format(band2_filename))
+
+band3_filename = landsat_item._data["id"] + "-b3.tiff"
+if not data_file_exists(band3_filename):
+    save_to_file(landsat_item.assets["B3"]["href"], band3_filename)
+else:
+    print("required data exists for {}".format(band3_filename))
+
+band4_filename = landsat_item._data["id"] + "-b4.tiff"
+if not data_file_exists(band4_filename):
+    save_to_file(landsat_item.assets["B4"]["href"], band4_filename)
+else:
+    print("required data exists for {}".format(band4_filename))
+
--- felicette/utils/file_manager.py
+++ felicette/utils/file_manager.py
@@ -1,5 +1,7 @@
 import tempfile
 import os
+import requests
+from tqdm import tqdm
 
 workdir = os.path.join(os.path.expanduser("~"), "felicette-data")
 data_path = os.path.join(workdir, "LC81390462020136")
@@ -8,12 +10,15 @@ if not os.path.exists(data_path):
     os.mkdir(data_path)
 
 
-def save_to_file(content, filename):
+def save_to_file(url, filename):
     file_path = os.path.join(data_path, filename)
-    with open(file_path, "w") as f:
-        data = "some data to be written to the file"
-        f.write(data)
-    f.close()
+    response = requests.get(url, stream=True)
+    with tqdm.wrapattr(open(file_path, "wb"), "write",
+                   miniters=1, desc=filename,
+                   total=int(response.headers.get('content-length', 0))) as fout:
+        for chunk in response.iter_content(chunk_size=4096):
+            fout.write(chunk)
+    fout.close()
 
 
 def data_file_exists(filename):
