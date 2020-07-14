13fe7876267a9fa5e4eb8bb7f04d76fc5721a152
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-14 21:37:00 +0530

    Make downloading of bands generic

--- felicette/landsat_handler.py
+++ felicette/landsat_handler.py
@@ -9,22 +9,11 @@ search = Search(
 landsat_item = search.items()[0]
 
 
-# save bands 2,3,4
-band2_filename = landsat_item._data["id"] + "-b2.tiff"
-if not data_file_exists(band2_filename):
-    save_to_file(landsat_item.assets["B2"]["href"], band2_filename)
-else:
-    print("required data exists for {}".format(band2_filename))
-
-band3_filename = landsat_item._data["id"] + "-b3.tiff"
-if not data_file_exists(band3_filename):
-    save_to_file(landsat_item.assets["B3"]["href"], band3_filename)
-else:
-    print("required data exists for {}".format(band3_filename))
-
-band4_filename = landsat_item._data["id"] + "-b4.tiff"
-if not data_file_exists(band4_filename):
-    save_to_file(landsat_item.assets["B4"]["href"], band4_filename)
-else:
-    print("required data exists for {}".format(band4_filename))
-
+# save bands generically
+bands = [2,3,4]
+for band in bands:
+    band_filename = landsat_item._data["id"] + "-b{}.tiff".format(band)
+    if not data_file_exists(band_filename):
+        save_to_file(landsat_item.assets["B{}".format(band)]["href"], band_filename)
+    else:
+        print("required data exists for {}".format(band_filename))
