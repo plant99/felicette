459747b60739226a061fc991101ffd9e521f633b
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-16 23:15:52 +0530

    Add geocoder util

--- felicette/utils/landsat_util.py
+++ felicette/utils/landsat_util.py
@@ -1,3 +1,5 @@
+import requests
+
 def find_first_landsat(items):
     for index, item in enumerate(items):
         if "LC" in item._data["id"]:
@@ -5,3 +7,10 @@ def find_first_landsat(items):
             return item
         else:
             print(item)
+
+def geocoder_util(city_name):
+    # call nominatim api
+    r = requests.get("https://nominatim.openstreetmap.org/search?city={}&format=json".format(name))
+    r_json = r.json()
+    # return lat, lon
+    return (r_json[0]["lat"], r_json[0]["lon"])
