55f36d9495704e4635404da8ea06e41e8e1c27e4
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-22 00:14:15 +0530

    Format with black

--- felicette/utils/image_processing_utils.py
+++ felicette/utils/image_processing_utils.py
@@ -2,18 +2,20 @@
 import numpy as np
 import cv2
 
+
 def find_max_area_index(contours):
     contour_areas = list(map(cv2.contourArea, contours))
     return contour_areas.index(max(contour_areas))
+
 
 def straighten_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.bitwise_not(gray)
     # threshold the image, setting all foreground pixels to 255 and all background pixels to 0
-    thresh = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     # flip data in threshold array
     thresh1 = thresh - 255
-    np.where(thresh1==-255, 0, thresh1) 
+    np.where(thresh1 == -255, 0, thresh1)
 
     # find angle of inclination
 
@@ -28,23 +30,29 @@ def straighten_image(image):
     (h, w) = image.shape[:2]
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h),flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    rotated = cv2.warpAffine(
+        image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE
+    )
     return rotated
+
 
 def remove_margin(rotated):
     # crop image to remove black margin
-    gray = cv2.cvtColor(rotated,cv2.COLOR_BGR2GRAY)
-    _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+    gray = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
     # # Now find contours in it. There will be only one object, so find bounding rectangle for it.
 
-    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
     max_contour_index = find_max_area_index(contours)
     cnt = contours[max_contour_index]
-    x,y,w,h = cv2.boundingRect(cnt)
+    x, y, w, h = cv2.boundingRect(cnt)
     # # Now crop the image, and save it into another file.
 
-    crop = rotated[y:y+h,x:x+w]
+    crop = rotated[y : y + h, x : x + w]
     return crop
+
 
 def process_sat_image(source_path, dest_path):
     # load the image from disk
@@ -54,5 +62,3 @@ def process_sat_image(source_path, dest_path):
 
     # write jpeg to destination
     cv2.imwrite(dest_path, cropped_image)
-
-    
