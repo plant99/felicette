ca2753445f3468379b23948b69e9f1f3dedf3a8d
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-23 10:11:19 +0530

    Improve and refactor prompt handler for preview image option

--- felicette/sat_downloader.py
+++ felicette/sat_downloader.py
@@ -7,6 +7,14 @@ from felicette.utils.sys_utils import exit_cli
 from felicette.constants import band_tag_map
 from felicette.utils.file_manager import save_to_file, data_file_exists, file_paths_wrt_id
 
+def handle_prompt_response(response):
+    if response in ["n", "N"]:
+        exit_cli("Why not try a different location next time? I'd suggest [link=https://en.wikipedia.org/wiki/Svalbard]Svalbard[/link] :)")
+    elif response in ["y", "Y", ""]:
+        return None
+    else:
+        exit_cli("[red]Sorry, invalid response. Exiting :([/red]")
+
 
 def search_landsat_data(coordinates, cloud_cover_lt):
     search = Search(
@@ -46,13 +54,8 @@ def preview_landsat_image(landsat_item):
     rprint("[blue]Preview image saved at:[/blue]")
     print(paths["preview"])
     # prompt a confirm option
-    response = input("Are you sure you want to see an enhanced version of the image at the path shown above? [y/n]")
-    if response in ["n", "N"]:
-        exit_cli("Why not try a different location next time? I'd suggest [link=https://en.wikipedia.org/wiki/Svalbard]Svalbard[/link] :)")
-    elif response in ["y", "Y"]:
-        return None
-    else:
-        exit_cli("[red]Sorry, invalid response. Exiting :([/red]")
+    response = input("Are you sure you want to see an enhanced version of the image at the path shown above? [Y/n]")
+    return handle_prompt_response(response)
 
 def download_landsat_data(landsat_item, bands=[2, 3, 4, 8]):
 
