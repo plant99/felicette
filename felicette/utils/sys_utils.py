67777ad2773177f6a6f66abe0db9e9df68067208
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-24 23:47:44 +0530

    Add util function to open generated images

--- felicette/utils/sys_utils.py
+++ felicette/utils/sys_utils.py
@@ -1,7 +1,18 @@
 import sys
+import os
 from rich import print as rprint
 
 
 def exit_cli(message):
     rprint("%s" % message)
     sys.exit(0)
+
+def display_file(file_name):
+    """
+    Open given file with default user program.
+    """
+    if sys.platform.startswith('linux'):
+        os.system('xdg-open %s' % file_name)
+
+    elif sys.platform.startswith('darwin'):
+        os.system('open %s' % file_name)
