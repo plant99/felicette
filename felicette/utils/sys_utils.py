8384811009cc0013074af9f28aaa09fc7fb7f9f2
Shivashis Padhi <shivashispadhi@gmail.com>
2020-07-26 16:43:52 +0530

    Add util function to remove a directory

--- felicette/utils/sys_utils.py
+++ felicette/utils/sys_utils.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 
 
 def exit_cli(print_func, message):
@@ -16,3 +17,15 @@ def display_file(file_name):
 
     elif sys.platform.startswith("darwin"):
         os.system("open %s" % file_name)
+
+def remove_dir(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            exit_cli(print, "")
